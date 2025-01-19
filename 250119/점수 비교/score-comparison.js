634ca2b200f5ec7363204f7df777c95a28885cab
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-19 15:52:57 +0900

    Title: 점수 비교, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

--- 250119/점수 비교/score-comparison.js
+++ 250119/점수 비교/score-comparison.js
@@ -6,10 +6,10 @@ let A = input[0].split(" ");
 let B = input[1].split(" ");
 
 let mathA = Number(A[0]);
-let engA = Number(A[0]);
+let engA = Number(A[1]);
 
 let mathB = Number(B[0]);
-let engB = Number(B[0]);
+let engB = Number(B[1]);
 
 
 console.log(mathA > mathB && engA > engB ? 1 : 0);
