38e0504751d97b2e1fe7753abdc2de50e30bdffd
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-19 15:15:26 +0900

    Title: 정수의 조건 여부 2, Time: 178ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250119/정수의 조건 여부 2/numbers-condition-2.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if(a === 5) {
+    console.log('A');
+} else if(a % 2 === 0) {
+    console.log('B');
+} else {
+    return;
+}
+
+
