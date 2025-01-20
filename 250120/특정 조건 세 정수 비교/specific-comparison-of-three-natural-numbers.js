cda9c9081ed0210af580532e1c4c6b763449c446
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-20 23:18:29 +0900

    Title: 특정 조건 세 정수 비교, Time: 181ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250120/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 250120/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -7,6 +7,6 @@ let c = Number(input[2]);
 
 let minimum = Math.min(a,b,c);
 let resultA = a === minimum ? 1 : 0;
-let resultB = a === b === c ? 1: 0;
+let resultB = a === b === c ? 1 : 0;
 
 console.log(resultA, resultB);
