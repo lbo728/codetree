06b9a35788f47e9e3537d4aacc101ec740b1646e
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-22 13:42:35 +0900

    Title: 장학금, Time: 193ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250122/장학금/scholarship.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let midResult = Number(input[0]);
+let finalResult = Number(input[1]);
+
+console.log(midResult);
+console.log(finalResult);
+
