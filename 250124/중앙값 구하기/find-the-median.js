39dd405d745dd3479d0988f31c29821d0ee279fa
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-24 23:42:59 +0900

    Title: 중앙값 구하기, Time: 180ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250124/중앙값 구하기/find-the-median.js
+++ 250124/중앙값 구하기/find-the-median.js
@@ -7,18 +7,32 @@ let c = Number(input[2]);
 
 let mid;
 
-if(a > b && b > c) {
+if(a > b && b > c && a > c)  {
     mid = b;
-} else if (b > c && c > a) {
+} else if (b > c && c > a && a < b) {
     mid = c;
-} else if (b > a && b < c ) {
-    mid = b;
-}  else if (a > b && b < c ){
+}  else if (a > b && b < c && a > c ) {
     mid = c;
-}   else {
+}
+else {
     mid = a;
 }
 
+
+// if(a > b && b > c && a > c)  {
+//     mid = b;
+// } else if (b > c && c > a && a < b) {
+//     mid = c;
+// } else if (b > a && b < c ) {
+//     mid = b;
+// }  else if (a > b && b < c ){
+//     mid = c;
+// }   else if (a > b && b < c)   {
+//     mid = a;
+// }
+// else {
+//     mid = a;
+// }
 
 
 console.log(mid);
