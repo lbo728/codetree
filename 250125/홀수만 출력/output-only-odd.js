dc364df702958e97732a32fce997675c20c353a3
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-25 18:16:54 +0900

    Title: 홀수만 출력, Time: 174ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250125/홀수만 출력/output-only-odd.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let result = "";
+
+for(i = 1; i >= a && i <= b; i++) {
+    if(i % 2 === 1) {
+        result += (i + " ");
+    }
+}
+
+console.log(result);
