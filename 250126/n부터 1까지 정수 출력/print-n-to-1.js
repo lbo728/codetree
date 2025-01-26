1fc742f5415c07153da47ef6c8fc93a45e4832d9
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-26 15:21:01 +0900

    Title: n부터 1까지 정수 출력, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250126/n부터 1까지 정수 출력/print-n-to-1.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let result = "";
+
+while(n >= 1) {
+    result += (n + " ");
+    n--;
+}
+
+
+console.log(result);
