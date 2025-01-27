21bcbceb8ee8fd232ab0fa803bd8c557b27a2245
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-27 21:34:28 +0900

    Title: 숫자의 증감, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250127/숫자의 증감/increasing-and-decreasing-numbers.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let c = input[0];
+let n = Number(input[1]);
+
+let result = "";
+
+if(c === 'A') {
+    for (i = 1; i <= n; i++ ) {
+        result += (i + " ");
+    }
+} else {
+    for (i = n; i >= 1; i-- ) {
+        result += (i + " ");
+    }
+}
+
+console.log(result);
