9654cc52a63949e62cad36d6a5544f3fc70348fc
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-27 21:36:54 +0900

    Title: 높은 수에서 낮은 수까지, Time: 184ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250127/높은 수에서 낮은 수까지/from-high-to-low.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let result = "";
+
+if(a > b) {
+    for(i = a; i >= b; i--) {
+        result += (i + " ");
+    }
+} else {
+    for(i = b; i >= a; i--) {
+        result += (i + " ");
+    }
+}
+
+console.log(result);
