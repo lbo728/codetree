d3a9f20bdafb7afe8e2a39d47376770984d8f894
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-29 20:57:29 +0900

    Title: 2 또는 3의 배수, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250129/2 또는 3의 배수/multiple-of-2-or-3.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+let n = fs.readFileSync(0).toString().trim();
+
+let result = "";
+
+for(i = 1; i <= n; i++) {
+    if(i % 2 === 0 || i % 3 === 0  ) {
+        result += (1 + " ");
+    } else {
+        result += (0 + " ");
+    }
+}
+
+console.log(result);
