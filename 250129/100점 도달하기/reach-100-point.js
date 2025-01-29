a2418a0adb2948297effa8f90441f5b969847c83
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-29 21:04:14 +0900

    Title: 100점 도달하기, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250129/100점 도달하기/reach-100-point.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+let n = fs.readFileSync(0).toString().trim();
+
+let result = ""
+
+for(i = n; i <= 100; i++) {
+    if(i >= 90) {
+        result += ("A" + " ");
+    }
+    else if(i >= 80) {
+        result += ("B" + " ");
+    }
+    else if(i >= 70) {
+        result += ("C" + " ");
+    }
+    else if(i >= 60) {
+        result += ("D" + " ");
+    } else {
+        result += ("F" + " ");
+    }
+}
+
+console.log(result);
