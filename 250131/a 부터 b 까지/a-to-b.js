f453da63f80c2468166af74be18e74fd8c9bdabe
byungsker <72309817+lbo728@users.noreply.github.com>
2025-01-31 22:50:11 +0900

    Title: a 부터 b 까지, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250131/a 부터 b 까지/a-to-b.js
@@ -0,0 +1,19 @@
+// 해설 풀이
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+
+let result = "";
+while (a <= b) {
+    result += a + " ";
+    if (a % 2 === 1) {
+        a *= 2;
+    }
+    else {
+        a += 3;
+    }
+}
+
+console.log(result);
