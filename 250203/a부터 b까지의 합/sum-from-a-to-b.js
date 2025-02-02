1be02338fbaaec499f4bd00dada742f673055ea5
byungsker <72309817+lbo728@users.noreply.github.com>
2025-02-03 00:14:21 +0900

    Title: a부터 b까지의 합, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250203/a부터 b까지의 합/sum-from-a-to-b.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let sum = 0;
+
+for(i = a; i <= b; i++) {
+        sum += i;
+}
+
+console.log(sum);
