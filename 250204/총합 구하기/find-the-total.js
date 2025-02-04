c2e8b6358298f0cdfc8c58ddaddd0e05bb6bf160
byungsker <72309817+lbo728@users.noreply.github.com>
2025-02-04 21:31:29 +0900

    Title: 총합 구하기, Time: 197ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250204/총합 구하기/find-the-total.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+let sumVal = 0;
+
+for(i = a; i <= b; i++) {
+    if(i % 6 === 0 && i % 8 !== 0) {
+        sumVal += i;
+    }
+}
+
+console.log(sumVal);
