aa2d167112fc98de479e2fe5afbd3cea7992bfcd
byungsker <72309817+lbo728@users.noreply.github.com>
2025-02-10 21:21:19 +0900

    Title: 조건에 부합하는 수, Time: 190ms, Memory: 15MB, Status: Passed - CodeTree

--- 250210/조건에 부합하는 수/number-that-meets-the-condition.js
+++ 250210/조건에 부합하는 수/number-that-meets-the-condition.js
@@ -1,4 +1,21 @@
 const fs = require('fs');
-let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(fs.readFileSync(0).toString().trim());
 
-console.log(input);
+let result = "";
+
+
+for(i = 1; i <= a; i++) {
+    if(i % 2 == 0 && i % 4 != 0) {
+        continue;
+    }
+    if(Math.floor(i / 8) % 2 == 0) {
+        continue;
+    }
+    if((i % 7) < 4) {
+        continue;
+    }
+    result += i + " ";
+}
+
+console.log(result);
+
